fece0c82c4791cb93a44c42df9d2b6ef47eefa31
Beyza KESER <beyzaaakeser@gmail.com>
2023-01-10 15:52:54 +0300

    10/01

---
+++ src/main/java/soru/Soru.java
@@ -0,0 +1,26 @@
+package soru;
+
+public class Soru {
+
+    public static void main(String[] args) {
+        //Bir String’ de tekrarlanan karakterleri yazdırmak için kod yazınız.
+        //Örneğin; accessories ´ ces
+
+        String s = "missisippi";
+        String d ="";
+
+        for (int i = 0; i <s.length() ;      i++) {
+            String c = s.substring(i,i+1);
+            if (s.indexOf(c)!=s.lastIndexOf(c)){
+
+                if (!d.contains(c)){
+                        d= d+c;
+                }
+            }
+        }
+
+        System.out.println(d);
+
+
+    }
+}
